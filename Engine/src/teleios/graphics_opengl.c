d35a48ce2f8188eb445488073d1d063a32ef80da
Bruno Araújo <bkraujo@outlook.com>
2023-12-08 16:27:00 -0300

    ...

--- Engine/src/teleios/graphics_opengl.c
+++ Engine/src/teleios/graphics_opengl.c
@@ -4,6 +4,22 @@
 #include "teleios/platform.h"
 #include "teleios/platform_detector.h"
 
+static b8 gl_context_load(void);
+
+b8 gl_initialize(const TLSpecification* spec) {
+    if (!gl_context_load()) {
+        TLERROR("gl_initialize: Failed to gladLoadGL");
+        return false;
+    }
+
+
+    TLDEBUG("GL_VENDOR: %s", glGetString(GL_VENDOR));
+    TLDEBUG("GL_RENDERER: %s", glGetString(GL_RENDERER));
+    TLDEBUG("GL_VERSION: %s", glGetString(GL_VERSION));
+
+    return true;
+}
+
 #ifdef TELEIOS_PLATFORM_WINDOWS
 #include <Windows.h>
 #include <GL/GL.h>
@@ -11,8 +27,7 @@
 static HGLRC context;
 #endif
 
-b8 gl_initialize(const TLSpecification* spec) {
-
+static b8 gl_context_load(void) {
 #ifdef TELEIOS_PLATFORM_WINDOWS
     PIXELFORMATDESCRIPTOR pfd = { 0 };
     pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
@@ -27,41 +42,34 @@ b8 gl_initialize(const TLSpecification* spec) {
 
     HDC hDc = GetDC(*((HWND*)tl_platform_window_handle()));
     i32 pixelFormat = ChoosePixelFormat(hDc, &pfd);
-    if (pixelFormat) {
-        if (!SetPixelFormat(hDc, pixelFormat, &pfd)) {
-            TLERROR("gl_initialize: Failed setting pixel format!");
-            return false;
-        }
+    if (pixelFormat == NULL) {
+        TLERROR("gl_context_load: Failed to ChoosePixelFormat");
+        return false;
+    }
+
+    if (!SetPixelFormat(hDc, pixelFormat, &pfd)) {
+        TLERROR("gl_context_load: Failed to SetPixelFormat");
+        return false;
     }
 
     context = wglCreateContext(hDc);
-    if (context) {
-        if (!wglMakeCurrent(hDc, context)) {
-            TLERROR("gl_initialize: Failed setting OpenGL context!");
-            return false;
-        }
-    }
-    else {
-        TLERROR("gl_initialize: Failed creating OpenGL context!");
-    }
-
-    if (!gladLoadGL()) {
-        TLERROR("gl_initialize: Failed to gladLoadGL");
+    if (context == NULL) return false;
+    if (!wglMakeCurrent(hDc, context)) {
+        TLERROR("gl_context_load: Failed to wglMakeCurrent");
         return false;
     }
+
+    return gladLoadGL();
+#else
+    return false;
 #endif
-
-    TLDEBUG("GL_VENDOR: %s", glGetString(GL_VENDOR));
-    TLDEBUG("GL_RENDERER: %s", glGetString(GL_RENDERER));
-    TLDEBUG("GL_VERSION: %s", glGetString(GL_VERSION));
-
-    return true;
 }
 
 b8 gl_terminamte(void) {
-
 #ifdef TELEIOS_PLATFORM_WINDOWS
-    wglDeleteContext(context);
+    if (context != NULL) {
+        wglDeleteContext(context);
+    }
 #endif
 
     return true;
