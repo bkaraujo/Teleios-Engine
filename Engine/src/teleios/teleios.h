a9e16ddd8095f7aa73f8587784798669b6522e90
Bruno Araújo <bkraujo@outlook.com>
2023-12-14 11:28:59 -0300

    The One Header

---
+++ Engine/src/teleios/teleios.h
@@ -0,0 +1,18 @@
+#ifndef TELELIOS_H
+#define TELELIOS_H
+
+#include "teleios/defines.h"
+#include "teleios/types.h"
+
+#include "teleios/container.h"
+#include "teleios/event.h"
+#include "teleios/filesystem.h"
+#include "teleios/input.h"
+#include "teleios/layerstack.h"
+#include "teleios/logger.h"
+#include "teleios/memory.h"
+#include "teleios/string.h"
+#include "teleios/time.h"
+#include "teleios/timer.h"
+
+#endif // TELELIOS_H
